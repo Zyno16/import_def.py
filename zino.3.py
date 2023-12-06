87097eb22af401d6408373b3407845e123d27945
Zyno16 <135590226+Zyno16@users.noreply.github.com>
2023-12-06 17:57:06 +0000

    Add files via upload

---
+++ zino.3.py
@@ -0,0 +1,16 @@
+
+import mydef
+print("===========")
+mydef.calc(7,3,"+")
+mydef.calc(7,3,"-")
+mydef.calc(7,3,"*")
+mydef.calc(7,3,"/")
+mydef.calc(7,0,"/")
+print("******************")
+
+mydef.hiall("hasan","ehab","amr","amine")
+
+print("++++++++++++++++++++")
+
+
+mydef.hiall("hasan","ehab","amr","amine")
